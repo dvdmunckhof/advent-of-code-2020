49f2beb489ec12620b638b1b5e4dd82c5c89937d
Dennis van den Munckhof <dennis_vd_munckhof@live.nl>
2020-12-10 23:17:40 +0100

    Day 10: Simplify solution

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,9 +1,7 @@
 class Day10(private val input: List<Int>) {
 
     fun solvePart1(): Int {
-        val joltages = input.toMutableList()
-        joltages.add(0) // add wall outlet
-        return joltages.sorted().zipWithNext().groupingBy { (a, b) -> b - a }
+        return input.plus(0).sorted().zipWithNext().groupingBy { (a, b) -> b - a }
             .eachCount().values.reduce { acc, value -> acc * (value + 1)}
     }
 

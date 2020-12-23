5d810f9c03d03ee4e54e28957a3dbc1ac45c626e
Dennis van den Munckhof <dennis_vd_munckhof@live.nl>
2020-12-23 11:15:02 +0100

    Day 19: Monster Messages - part 2

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,38 +1,48 @@
-import java.util.regex.Pattern
+class Day19(input: String) {
+    private val inputRules: List<String>
+    private val inputMessages: List<String>
 
-class Day19(private val input: String) {
+    init {
+        val (rules, messages) = input.splitOnce("\n\n")
+        inputRules = rules.split("\n")
+        inputMessages = messages.split("\n")
+    }
 
     fun solvePart1(): Int {
-        val (rules, messages) = input.splitOnce("\n\n")
-        val regex = getPattern(rules, "0")
-        return messages.split("\n").count { regex.matcher(it).matches() }
+        val rules = parseRules(inputRules)
+        return inputMessages.count { matchRule(rules, it, listOf(0)) }
     }
 
     fun solvePart2(): Int {
-        error("No solution found")
+        val updatedRules = listOf("8: 42 | 42 8", "11: 42 31 | 42 11 31")
+        val rules = parseRules(inputRules + updatedRules).toSortedMap()
+        return inputMessages.count { matchRule(rules, it, listOf(0)) }
     }
 
-    private fun getPattern(rules: String, root: String): Pattern {
-        val inputRules = rules.split('\n').associate { it.splitOnce(": ") }
-        val rulesCache = mutableMapOf<String, String>()
-
-        fun getRule(name: String): String = rulesCache.getOrPut(name) {
-            val parts = inputRules.getValue(name).split(' ')
-            val containsOr = parts.contains("|")
-            buildString {
-                if (containsOr) append("(")
-                for (part in parts) {
-                    val result = when {
-                        part == "|" -> "|"
-                        part.startsWith("\"") -> part.trim('"')
-                        else -> getRule(part)
-                    }
-                    append(result)
-                }
-                if (containsOr) append(")")
-            }
+    private fun matchRule(rules: Map<Int, Rule>, message: String, stack: List<Int>): Boolean {
+        if (stack.isEmpty()) {
+            return message.isEmpty()
         }
 
-        return Pattern.compile("^" + getRule(root) + "$")
+        return when (val rule = rules.getValue(stack.first())) {
+            is Rule.Literal -> message.startsWith(rule.value) && matchRule(rules, message.drop(1), stack.drop(1))
+            is Rule.Reference -> rule.groups.any { group -> matchRule(rules, message, group + stack.drop(1)) }
+        }
+    }
+
+    private fun parseRules(rules: List<String>): Map<Int, Rule> {
+        return rules.associate { rule ->
+            val (key, value) = rule.splitOnce(": ")
+            key.toInt() to if (value.startsWith('"')) {
+                Rule.Literal(value.elementAt(1))
+            } else {
+                Rule.Reference(value.split(" | ").map { it.split(" ").map(String::toInt) })
+            }
+        }
+    }
+
+    sealed class Rule {
+        class Literal(val value: Char) : Rule()
+        class Reference(val groups: List<List<Int>>) : Rule()
     }
 }
--- src/test/kotlin/Day19Test.kt
+++ src/test/kotlin/Day19Test.kt
@@ -7,7 +7,7 @@ class Day19Test {
 
     @Test
     fun `Part 1 - Example`() {
-        val input = Resources.readString("day19_example_input.txt")
+        val input = Resources.readString("day19_example_part1_input.txt")
         val answer = Day19(input).solvePart1()
 
         Assertions.assertEquals(2, answer)
@@ -19,5 +19,21 @@ class Day19Test {
         val answer = Day19(input).solvePart1()
 
         Assertions.assertEquals(213, answer)
+    }
+
+    @Test
+    fun `Part 2 - Example`() {
+        val input = Resources.readString("day19_example_part2_input.txt")
+        val answer = Day19(input).solvePart2()
+
+        Assertions.assertEquals(12, answer)
+    }
+
+    @Test
+    fun `Part 2 - Actual`() {
+        val input = Resources.readString("day19_actual_input.txt")
+        val answer = Day19(input).solvePart2()
+
+        Assertions.assertEquals(325, answer)
     }
 }
